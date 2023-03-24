c13b3100d23a5420499c55a07cf1eb30f929710a
Edwin Agudelo <117861779+EA-Quorum@users.noreply.github.com>
2023-03-24 16:17:25 -0500

    Advance

---
+++ README.md
@@ -0,0 +1,10 @@
+# Libreria SBI
+
+Esto es un intento por tener ciertas funcionalidades concentradas en un solo repositorio
+
+## Dependencias
+
+Este proyecto tiene las siguientes dependencias:
+
+ - LibPQ
+ - OCILIB
--- src/CUtiles.cpp
+++ src/CUtiles.cpp
@@ -1,4 +1,4 @@
-#include "include/CUtiles.h"
+#include "CUtiles.h"
 
 using namespace SBI;
 
@@ -12,17 +12,11 @@ CUtiles::~CUtiles()
     //dtor
 }
 
-char* CUtiles::traerFechaActual(){
-    char* retorno = new char[20];
-    time_t t;
-    struct tm* ltm;
-    time(&t);
-    ltm = localtime(&t);
-    strftime(retorno,20,"%Y%m%d%H%M%S",ltm);
-    return retorno;
+const char* CUtiles::traerFechaActual(){
+    return CUtiles::traerFechaActual("%Y%m%d%H%M%S");
 }
 
-char* CUtiles::traerFechaActual(char* formato){
+const char* CUtiles::traerFechaActual(char* formato){
     char* retorno = new char[20];
     time_t t;
     struct tm* ltm;
@@ -32,61 +26,76 @@ char* CUtiles::traerFechaActual(char* formato){
     return retorno;
 }
 
-int CUtiles::traerListaArchivos(const string ruta, const string mascara, vector<string> &archivos){
-    // Variables
+int CUtiles::traerListaArchivos(const std::string ruta, const std::string mascara, std::vector<std::string> &archivos){
     struct stat sb;
     int cont = 0;
-    //sprintf(def_mascara,"%s",mascara.c_str());
-    string sep = "/";
-    #ifdef _WIN32
-        sep = "\\";
+    std::string sep = CUtiles::carSeparador();
+
+    #ifdef _DEBUG
+    std::cout<<"Validando:"<<ruta<<std::endl;
     #endif
 
-    cout<<"Validando:"<<ruta<<endl;
-
     // Valido si el directorio existe
-    int itmp = stat(ruta.c_str(), &sb);
+    int itmp = CUtiles::dirExiste(ruta.c_str());
     if(itmp < 0){
-        cout<<"Error:"<<strerror(errno)<<">>"<<ruta.c_str()<<endl;
+        #ifdef _DEBUG
+        std::cout<<"Error:"<<strerror(errno)<<">>"<<ruta.c_str()<<std::endl;
+        #endif
         return 0;
     }
-    // Ahora bifurco segun el entorno en el que este
+    // Ahora bifurco segun el entorno en el que este compilando
     #ifdef _WIN32
-    string ruta_arch;
-    char *def_mascara = new char[strlen(mascara.c_str()) + 10];
-    sprintf(def_mascara,"(%s)(.*)",mascara.c_str());
-    regex val_mascara("(.+?)(\\.BGH)");
-    DIR *dp;
-    struct dirent *dirp;
-    if((stat(ruta.c_str(), &sb) == 0)){
-        if( (S_ISDIR(sb.st_mode)) ){
-            if( (dp = opendir(ruta.c_str())) != NULL ){
-                while( (dirp = readdir(dp)) != NULL ){
-                    ruta_arch = ruta + sep + string(dirp->d_name);
-                    stat(ruta_arch.c_str(), &sb);
-                    if(S_ISREG(sb.st_mode)){
-                        if(regex_match(string(dirp->d_name),val_mascara)){
-                        //if(regex_match("*",val_mascara)){
-                            archivos.push_back(ruta_arch);
-                            cont++;
+        #ifdef _MSC_VER
+        struct _finddata_t c_file;
+        intptr_t hFile;
+        char* current_directory ;
+        if( !((current_directory = _getcwd(NULL, o)) == NULL) ){
+            _chdir(ruta.c_str());
+            if( (hFile = _findfirst(mascara.c_str(), &c_file)) != -1){
+                do{
+                    archivos.push_back(std::string(c_file.name));
+                    cont++;
+                }while(_findnext(hFile, &c_file) == 0);
+            }
+            _chdir(current_directory);
+        }
+        #else
+        std::string ruta_arch;
+        char *def_mascara = new char[strlen(mascara.c_str()) + 10];
+        sprintf(def_mascara,"(%s)(.*)",mascara.c_str());
+        std::regex val_mascara("(.+?)(\\.BGH)");
+        DIR *dp;
+        struct dirent *dirp;
+        if((stat(ruta.c_str(), &sb) == 0)){
+            if( (S_ISDIR(sb.st_mode)) ){
+                if( (dp = opendir(ruta.c_str())) != NULL ){
+                    while( (dirp = readdir(dp)) != NULL ){
+                        ruta_arch = ruta + sep + std::string(dirp->d_name);
+                        stat(ruta_arch.c_str(), &sb);
+                        if(S_ISREG(sb.st_mode)){
+                            if(regex_match(std::string(dirp->d_name),val_mascara)){
+                            //if(regex_match("*",val_mascara)){
+                                archivos.push_back(ruta_arch);
+                                cont++;
+                            }
                         }
                     }
                 }
             }
-        }
-    }
+        }    
+        #endif
     #else
     try{
         glob_t gres;
         char *cstr_ruta = new char[sizeof(char) * (ruta.size() + sep.size() + mascara.size()) + 1];
-        //const string ruta_completa = (ruta + sep + mascara);
+        //const std::string ruta_completa = (ruta + sep + mascara);
         sprintf(cstr_ruta,"%s%s%s",ruta.c_str(),sep.c_str(), mascara.c_str());
         //int gtmp = glob(ruta_completa.c_str(), 0, NULL, &gres);
         int gtmp = glob(cstr_ruta, 0, NULL, &gres);
         if( gtmp == 0){
             for(size_t i = 0; i< gres.gl_pathc; i++){
-                //ruta_arch = ruta + sep + string(*(gres.gl_pathv));
-                archivos.push_back(string(*(gres.gl_pathv)));
+                //ruta_arch = ruta + sep + std::string(*(gres.gl_pathv));
+                archivos.push_back(std::string(*(gres.gl_pathv)));
                 cont++;
             }
         }
@@ -99,12 +108,11 @@ int CUtiles::traerListaArchivos(const string ruta, const string mascara, vector<
     }catch(exception &ex){
         cout<<"Error al buscar archivos:"<<ex.what()<<endl;
     }
-    #endif // _WIN32
+    #endif
     return cont;
 }
 
-int CUtiles::traerListaArchivos(string ruta, string mascara, vector<string> &archivos, int ord){
-    //
+int CUtiles::traerListaArchivos(std::string ruta, std::string mascara, std::vector<std::string> &archivos, int ord){
     int tcnt = traerListaArchivos(ruta, mascara, archivos);
     if (ord != 0){
         sort(archivos.begin(),archivos.end());
@@ -112,31 +120,32 @@ int CUtiles::traerListaArchivos(string ruta, string mascara, vector<string> &arc
     return tcnt;
 }
 
-void CUtiles::split(string cadena, char sep, vector<string> &retorno){
-    string tsep(1,sep);
+int CUtiles::traerListaArchivos(std::string ruta, std::vector<std::string> &archivos){
+    std::string mascara = "*";
+    int tcnt = traerListaArchivos(ruta, mascara, archivos);
+    return tcnt;
+}
+
+int CUtiles::split(const std::string& cadena, const char& sep, std::vector<std::string> &retorno){
+    std::string tsep(1,sep);
     size_t substart = 0;
     size_t cant_sep;
     retorno.clear();
-    while( (cant_sep = cadena.find(tsep, substart)) != string::npos ){
+    while( (cant_sep = cadena.find(tsep, substart)) != std::string::npos ){
         retorno.push_back(cadena.substr(substart, cant_sep - substart));
         substart = cant_sep + 1;
     }
     retorno.push_back(cadena.substr(substart, cadena.size()));
-    return;
+    return retorno.size();
 }
 
-string CUtiles::getFileName(const string& s) {
-
-   char sep = '/';
-   #ifdef _WIN32
-    sep = '\\';
-   #endif
+std::string CUtiles::getFileName(const std::string& s) {
+   char sep = CUtiles::carSeparador()[0];
    size_t i = s.rfind(sep, s.length());
-   if (i != string::npos) {
+   if (i != std::string::npos) {
       return(s.substr(i+1, s.length() - i));
    }
    return("");
-
 }
 
 void CUtiles::retiraEspacios(char* str){
@@ -161,17 +170,20 @@ void CUtiles::retiraCaracter(char* str, char car){
     str[j] = '\0';
 }
 
-void CUtiles::cambiaCaracter(char* str, char car_o, char car_d ){
+int CUtiles::cambiaCaracter(char* str, char car_o, char car_d ){
     unsigned int i=0;
+    int cambiados = 0;
     while( str[i] ){
         if(str[i] == car_o){
             str[i] = car_d;
+            cambiados++;
         }
         i++;
     }
+    return cambiados;
 }
 
-char* CUtiles::carSeparador(){
+const char* CUtiles::carSeparador(){
     char *sep = new char[2];
     strcpy(sep,"/");
     #ifdef _WIN32
@@ -181,7 +193,7 @@ char* CUtiles::carSeparador(){
 }
 
 
-char* CUtiles::refPago(char* num){
+const char* CUtiles::calcularReferencia(const char* num){
     char* retorno;
     int largo = strlen(num);
     int suma = 0;
@@ -205,7 +217,11 @@ char* CUtiles::refPago(char* num){
 int CUtiles::crearDirectorio(char* ruta){
     int retorno = 0;
     int estado = 0;
+    #ifndef _WIN32
     estado = mkdir(ruta,S_IRWXU | S_IRWXG | S_IRWXO);
+    #else
+    estado = mkdir(ruta);
+    #endif
     if(estado != 0)
         retorno = -1;
     return retorno;
@@ -223,15 +239,21 @@ int CUtiles::dirExiste(const char *path){
         return 0;
 }
 
-string CUtiles::getOSUsername(){
-    string retorno(getenv("USER"));
+const std::string CUtiles::getOSUsername(){
+    std::string retorno(getenv("USER"));
     return retorno;
 }
 
 int CUtiles::valAcceso(const char *path){
     int retorno = 0;
+    #ifndef _WIN32
     if( access(path, R_OK) == -1 ){
         retorno = 1;
     }
+    #else
+    if( access(path, 04) == -1 ){
+        retorno = 1;
+    }
+    #endif
     return retorno;
 }
