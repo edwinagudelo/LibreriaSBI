14b59b39ede85ba2a493a8a1d68e7fff09999de7
Edwin Agudelo <117861779+EA-Quorum@users.noreply.github.com>
2024-03-08 15:46:44 -0500

    Added Catch2

--- tests/configura_test.cpp
+++ tests/configura_test.cpp
@@ -1,1 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
+
+#include <memory>
+
+#include "CConfigura.h"
+
+TEST_CASE("Check The configuration") {
+    std::unique_ptr<CConfigura> config = std::make_unique<CConfigura>();
+    auto setparam = config->setParam("prueba", "123");
+    REQUIRE(setparam == true);
+}
