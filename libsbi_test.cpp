e912c6ef4747071638d11b9dfbc847399d23eb18
Edwin Agudelo <117861779+EA-Quorum@users.noreply.github.com>
2024-03-07 20:55:39 -0500

    Ajustada la parte de configuracion

--- libsbi_test.cpp
+++ libsbi_test.cpp
@@ -8,13 +8,43 @@
 */
 
 #include <iostream>
+#include <memory>
 
-// #include "CConexion.h"
-// #include "CConfigura.h"
-#include "CUtiles.h"
+#include "CConfigura.h"
+
+void test_configura(){
+    std::cout << "Iniciando pruebas de CConfigura" << std::endl;
+    std::unique_ptr<CConfigura> config = std::make_unique<CConfigura>();
+    auto setparam = config->setParam("prueba", "123");
+    if (!setparam) {
+        std::cout << "Error: El parametro no se pudo setear" << std::endl;
+    }
+    else {
+        std::cout << "Parametro seteado correctamente" << std::endl;
+    }
+    config->setModif(false);
+    setparam = config->setParam("prueba", "345");
+    if (setparam) {
+        std::cout << "Error: El parametro se dejo modificar" << std::endl;
+    }
+    else {
+        std::cout << "El parametro no se dejo modifiacar" << std::endl;
+    }
+    auto param = config->getParam("prueba");
+    if (param != "123") {
+        std::cout << "Error: El parametro no es el esperado -- " << param << std::endl;
+    }
+    else {
+        std::cout << "El parametro es el esperado [" << param << "]" << std::endl;
+    }
+}
 
 
 int main(){
     std::cout << "Libreria SBI" << std::endl;
+    std::cout << "Probando" << std::endl;
+
+    test_configura();
+    
     return 0;
 }
--- src/CConfigura.cpp
+++ src/CConfigura.cpp
@@ -1,65 +1,49 @@
-#include "include/CConfigura.h"
+#include "CConfigura.h"
 
 CConfigura::CConfigura()
 {
     //ctor
-    modificable = 0;
+    modificable = false;
 }
 
 CConfigura::~CConfigura()
 {
     //dtor
+    // just free memory
+    this->configuraciones.clear();
 }
 
-std::string CConfigura::getParam(std::string llave){
+std::string CConfigura::getParam(const std::string& llave){
     std::string retorno = "";
     if(configuraciones.count(llave) != 0){ // la llave existe
-        std::map <std::string,int> mptmp = configuraciones[llave];
-        retorno = mptmp.begin()->first;
+        return configuraciones[llave];
     }
     return retorno;
 }
 
-bool CConfigura::setParam(std::string llave, std::string valor){
+bool CConfigura::setParam(const std::string& llave, const std::string& valor){
     if(configuraciones.count(llave) != 0){
         if(modificable == 0){
             return false;
         }
         std::cerr << "Se sobreescribira" << llave <<std::endl;
     }
-    std::map<std::string,int> tmp;
-    tmp[valor] = (int)(this->NO_TIPO);
-    configuraciones[llave] = tmp;
+    configuraciones.insert(std::pair<std::string, std::string>(llave, valor)) ;
     return true;
 }
 
-bool CConfigura::setParam(std::string llave, std::string valor, int tipo){
-    if(configuraciones.count(llave) != 0){
-        if(modificable == 0){
-            return false;
-        }
-        std::cerr << "Se sobreescribira" << llave <<std::endl;
-    }
-    std::map<std::string,int> tmp;
-    tmp[valor] = tipo;
-    configuraciones[llave] = tmp;
-    return true;
-}
-
-int CConfigura::getModif(){
+bool CConfigura::getModif(){
     return modificable;
 }
 
-void CConfigura::setModif(int modif){
+void CConfigura::setModif(bool modif){
     modificable = modif;
     return;
 }
 
 void CConfigura::mostrar(){
-    for(std::map<std::string , std::map<std::string, int> >::const_iterator ite = configuraciones.begin() ; ite != configuraciones.end() ; ite++){
-        for(std::map<std::string, int>::const_iterator iti = ite->second.begin() ; iti != ite->second.end() ; iti++){
-            std::cout << "Parametro:" << ite->first << "\tValor:" << iti->first << "\tTipo:" << iti->second << std::endl;
-        }
+    for(std::map<std::string , std::string >::const_iterator ite = configuraciones.begin() ; ite != configuraciones.end() ; ite++){
+        std::cout << "Parametro:" << ite->first << "\tValor:" << ite->second << std::endl;
     }
 }
 
